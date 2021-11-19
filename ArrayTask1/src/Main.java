f983d56490b25a16b97868e771b128e89ac61aac
felixwugithub <94074190+felixwugithub@users.noreply.github.com>
2021-11-19 14:52:18 -0800

    Completed Array Practice Task 1

---
+++ ArrayTask1/src/Main.java
@@ -0,0 +1,28 @@
+//        •Create an array that holds 20 positions, give each index a
+//        random value between 1-6, then use a for loop to count
+//        how many 1's were randomly generated in  the array
+
+public class Main {
+    public static void main(String[] args) {
+
+        int count1 = 0;
+
+        int[] ints = new int[20];
+
+        for (int i = 0; i < ints.length; i++) {
+
+            ints[i] = (int) ((Math.random() * 6) + 1);
+            if(ints[i] == 1){
+                count1++;
+            }
+        }
+        System.out.println("1 is found " + count1 + " time(s) in this array.");
+
+
+
+
+
+
+
+    }
+}
